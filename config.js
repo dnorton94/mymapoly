01e51b9980b97fd2f03d490076db29cf664be81c
dnorton94 <dominiconorton@gmail.com>
2019-12-11 17:15:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var apiKey = '';
-var geofencingAdminKey = '';
-var geofencingProjectId = '';
+var apiKey = 'TyKNUoGCT45fxZCkuTsk3Br70x2dJVWO';
+var geofencingAdminKey = 'Y23wbGcrqZmB7Yaj2U0YzjosEgpfZmnekMfKngwJZWta9ivC';
+var geofencingProjectId = '236ce422-8373-4f1b-a85b-9a99680410e1';
